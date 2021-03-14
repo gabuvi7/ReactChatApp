5bfd6c06680e7e94aa7f42c01d1b223ae550cae1
Gabriel <uviedo_gabriel@hotmail.com>
2021-03-14 17:39:10 -0300

    added login and chats for users

--- react-chat-app/src/App.js
+++ react-chat-app/src/App.js
@@ -1,3 +1,20 @@
-import { ChatEngine } from 'react-chat-engine' ;
-import './App.css';
+import { ChatEngine } from "react-chat-engine";
+import "./App.css";
+import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
+import { PROJECT_ID } from "./services/settings";
 
+export default function App() {
+  const username = localStorage.getItem("username");
+  const password = localStorage.getItem("password");
+  if (!localStorage.getItem("username")) return <LoginForm />;
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={PROJECT_ID}
+      userName={username}
+      userSecret={password}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
+  );
+}
